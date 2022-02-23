f73dcfcee916d8b8569f42cfc0a26599990f24fd
OscarJava90210 <ashatbek572@gmail.com>
2022-02-23 07:38:14 +0000

    DocumentFactory.java edited online with Bitbucket

--- src/main/java/Factory/DocumentFactory.java
+++ src/main/java/Factory/DocumentFactory.java
@@ -3,13 +3,11 @@ package Factory;
 /**
  * Интерфейс для генерации классов Model.Document
  */
-
-
 public interface DocumentFactory<T> {
-    /**
-     * @param <T> фабрика создания определенного типа документа
+   
+   /**
+     * @param <T> фабрика для создания определенного типа документа
      */
     T createDocument();
-
 
 }
