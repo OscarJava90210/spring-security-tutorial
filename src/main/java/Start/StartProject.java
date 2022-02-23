e69bee4667c707ea2acd1f18d160a261e23d0e29
OscarJava90210 <ashatbek572@gmail.com>
2022-02-23 10:00:51 +0000

    StartProject.java edited online with Bitbucket

--- src/main/java/Start/StartProject.java
+++ src/main/java/Start/StartProject.java
@@ -3,14 +3,11 @@ package Start;
 import Model.Document;
 import Service.ServiceReport;
 import Service.StorageList;
-
-
 import java.util.ArrayList;
 
 /*Класс для старта программы
  */
 public class StartProject {
-
     public static void main(String[] args) {
         int countDocuments = 10;
         ArrayList<Document> documentArrayList = StorageList.creater(countDocuments);
