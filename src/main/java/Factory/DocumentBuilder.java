087c58bc3b21432ab5795cfe2048a1de841093ba
OscarJava90210 <ashatbek572@gmail.com>
2022-02-23 07:32:39 +0000

    DocumentBuilder.java edited online with Bitbucket

--- src/main/java/Factory/DocumentBuilder.java
+++ src/main/java/Factory/DocumentBuilder.java
@@ -1,18 +1,24 @@
 package Factory;
 
 import java.util.Date;
+
 /**
  * Интерфейс для генерации документов
  *
- * @param <T> поля builder
+ * @param <T> поле builder
  */
 public interface DocumentBuilder<T> {
 
     T setId (int id);
+    
     T setRegNumDoc (int regNumDoc);
+    
     T setDateRegDoc (int setDateRegDoc);
+    
     T setAuthorOfDoc (String authorOfDoc);
+    
     T setNameDoc (String nameDoc);
+    
     T setTextDoc (String textDoc);
 
 
