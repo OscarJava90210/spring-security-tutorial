86f2028fdf5603b1aec35a715637ea165478c9b4
OscarJava90210 <ashatbek572@gmail.com>
2022-02-23 09:34:47 +0000

    ServiceGenerator.java edited online with Bitbucket

--- src/main/java/Service/ServiceGenerator.java
+++ src/main/java/Service/ServiceGenerator.java
@@ -9,22 +9,18 @@ import java.util.concurrent.ThreadLocalRandom;
 public   class ServiceGenerator  {
 
     public static Random random = new Random();
-
     /*Генерация числа*/
     public static Date randomInt() {
         return new Date(System.currentTimeMillis() - ThreadLocalRandom.current().nextInt(10 * 1000));
     }
-
     /*Генерация даты */
     public static Date randomDate() {
         return new Date(System.currentTimeMillis() - ThreadLocalRandom.current().nextLong(10 * 1000));
     }
-
     /*Генерация элемента из листа*/
     public static <T> T randomFromList(ArrayList<T> list) {
         return list.get(random.nextInt(list.size()));
     }
-
 }
 
 
