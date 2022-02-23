35072247e6272a336485ca619a590dec809e8a1e
OscarJava90210 <ashatbek572@gmail.com>
2022-02-23 09:29:40 +0000

    ServiceGenerator.java edited online with Bitbucket

--- src/main/java/Service/ServiceGenerator.java
+++ src/main/java/Service/ServiceGenerator.java
@@ -1,6 +1,8 @@
 package Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 /*Класс для генерации чисел и дат*/
