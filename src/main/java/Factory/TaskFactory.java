1943beb1aa41365067ca4400b31e4784435962d8
OscarJava90210 <ashatbek572@gmail.com>
2022-02-23 08:40:26 +0000

    TaskFactory.java edited online with Bitbucket

--- src/main/java/Factory/TaskFactory.java
+++ src/main/java/Factory/TaskFactory.java
@@ -4,18 +4,25 @@ import Model.TaskDocument;
 import Service.ServiceGenerator;
 import Service.ServiceList;
 
-/**
- * Класс - фабрика заданий
+ /**
+ * Класс для создания документа Задачи
+ *
+ * @return объект - документ задачи
  */
 public class TaskFactory implements DocumentFactory<TaskDocument> {
 
+   /**
+     * метод генерации документа Задачи
+     *
+     * @return получение документа Задачи
+     */
     @Override
     public TaskDocument createDocument() {
         return TaskDocument.newTaskBuilder()
                 .setDateTask(ServiceGenerator.randomDate())
                 .setController("Контролер")
                 .setPeriodOfTask(ServiceGenerator.randomDate())
-                .setExecutor("j")
+                .setExecutor("Исполнитель")
                 .setStatusControl("Текущий статус")
                 .setType("Задача")
                 .setId(ServiceGenerator.randomInt())
