5725f31340996d17234861703e654e94c891e73a
OscarJava90210 <ashatbek572@gmail.com>
2022-03-07 09:46:21 +0000

    IncomingRepository.java edited online with Bitbucket

--- src/main/java/ServiceRepo/IncomingRepository.java
+++ src/main/java/ServiceRepo/IncomingRepository.java
@@ -3,12 +3,14 @@ package ServiceRepo;
 import Model.Document;
 import Exception.DocumentExistsException;
 import Service.StorageList;
-
 import java.util.ArrayList;
 
-/**Repository для класса IncomingRepository
+/**
+ * Репозиторий для класса IncomingRepository
+ *
+ * @author AAM
  */
-public class IncomingRepository <T> extends StorageList {
+public class IncomingRepository<T> extends StorageList {
 
     /**
      * Конструктор IncomingRepository
@@ -19,22 +21,25 @@ public class IncomingRepository <T> extends StorageList {
     /**
      * Поле объект для instance
      */
-     public static IncomingRepository instance;
+    public static IncomingRepository instance;
 
-    /**Для получения Instance**/
-    public static IncomingRepository getInstance()  {
+    /**
+     * @method для получения Instance
+     **/
+    public static IncomingRepository getInstance() {
         return instance;
     }
 
     /**
-     * Метод для добавления документа
+     * @method для добавления документа в storage list
      */
     public void addDoc(Document document) throws DocumentExistsException {
         verification(document);
         documentArrayList.add(document);
     }
-    /*Удаление*/
-    public void remove (Document document) throws DocumentExistsException {
+
+    /*@method Удаление storage list */
+    public void remove(Document document) throws DocumentExistsException {
         boolean exist = false;
         for (int i = 0; i < documentArrayList.size(); i++) {
             if (documentArrayList.get(i).equals(document)) {
@@ -49,16 +54,17 @@ public class IncomingRepository <T> extends StorageList {
     }
 
     /*Проверка существующего рег.номера*/
-    public void verification (Document document) throws DocumentExistsException {
+    public void verification(Document document) throws DocumentExistsException {
         for (Document doc : documentArrayList) {
             if (doc.getRegNumDoc() == document.getRegNumDoc()) {
-                throw new DocumentExistsException("Рег. номер уже существует");
+                throw new DocumentExistsException("Регистрационный  номер уже существует");
             }
         }
     }
 
     /**
-     * Метод для storage list*/
-    ArrayList<Document> documentArrayList = new ArrayList<Document>(){
+     * @method storage list
+     */
+    ArrayList<Document> documentArrayList = new ArrayList<Document>() {
     };
 }
