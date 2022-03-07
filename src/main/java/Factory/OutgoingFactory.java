4b2b05474adcfd65af88f19934c1a7d37cafb11b
OscarJava90210 <ashatbek572@gmail.com>
2022-03-07 08:56:37 +0000

    OutgoingFactory.java edited online with Bitbucket

--- src/main/java/Factory/OutgoingFactory.java
+++ src/main/java/Factory/OutgoingFactory.java
@@ -4,16 +4,17 @@ import Model.OutgoingDocument;
 
 /**
  * Класс - фабрика исходящих документов
- *
  * @return объект - исходящий документ
+ * @author AAM
  */
 public class OutgoingFactory implements DocumentFactory<OutgoingDocument> {
   
-  /**
-     * метод генерации исходящего документа
-     *
-     * @return получение исходящего документа
-     */
+/**
+  * метод генерации исходящего документа
+  * {@inheritDoc}
+  *
+  * @return получение исходящего документа
+  */
   @Override
     public OutgoingDocument createDocument() {
         return OutgoingDocument.newOutgoingBuilder()
