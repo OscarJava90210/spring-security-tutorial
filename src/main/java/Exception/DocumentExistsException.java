a38f6efeb8192ba242fa8e160412e09813d7491f
OscarJava90210 <ashatbek572@gmail.com>
2022-03-07 08:45:40 +0000

    DocumentExistsException.java edited online with Bitbucket

--- src/main/java/Exception/DocumentExistsException.java
+++ src/main/java/Exception/DocumentExistsException.java
@@ -1,8 +1,10 @@
 package Exception;
 
 /**
- * Класс для ошибок при создании документов
- */
+  * Класс для ошибок при создании документов
+  *
+  * @author AAM
+  */
 public class DocumentExistsException extends Exception {
 
  /**
