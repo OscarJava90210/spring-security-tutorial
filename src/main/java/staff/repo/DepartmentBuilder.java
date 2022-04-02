33c3ae66fcc09c028256cb22a1771f82b3f4717c
OscarJava90210 <ashatbek572@gmail.com>
2022-04-03 00:32:25 +0500

    builder

---
+++ src/main/java/staff/repo/DepartmentBuilder.java
@@ -0,0 +1,39 @@
+package staff.repo;
+
+import staff.Model.Department;
+
+/**
+ * Реализация Buildet для класса Department
+ *
+ * @author AAM
+ */
+public class DepartmentBuilder {
+    private String fullName;
+    private String shortName;
+    private String chief;
+    private String contact;
+
+    public DepartmentBuilder setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public DepartmentBuilder setShortName(String shortName) {
+        this.shortName = shortName;
+        return this;
+    }
+
+    public DepartmentBuilder setChief(String chief) {
+        this.chief = chief;
+        return this;
+    }
+
+    public DepartmentBuilder setContact(String contact) {
+        this.contact = contact;
+        return this;
+    }
+
+    public Department createDepartment() {
+        return new Department(fullName, shortName, chief, contact);
+    }
+}
